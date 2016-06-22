a5da2df284a300057f6062c9e0986e5c2732e405
setkyar <setkyar16@gmail.com>
2016-06-22 23:38:04 +0630

    WIP on github battel day 6 | (not so much very well today)

---
+++ app/containers/ConfirmBattleContainer.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ConfirmBattle = require('../components/ConfirmBattle')
+
+var ConfirmBattleContainer = React.createClass({
+	contextTypes: {
+		router: React.PropTypes.object.isRequired
+	},
+	getInitialState: function() {
+		return {
+			isLoading: true,
+			playerInfo: []
+		}
+	},
+	render: function() {
+		<ConfirmBattle />
+	}
+});
+
+module.exports = ConfirmBattleContainer;
